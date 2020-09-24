03a041dc487ff2338db497ed4394c9c9e17b4007
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-09-24 22:16:16 +0700

    membuat api get untuk list image, sebelumnya api post untuk image

--- routes/media.js
+++ routes/media.js
@@ -6,6 +6,24 @@ const base64Img = require('base64-img');
 
 const { Media } = require('../models');
 
+router.get('/', async (req,res) => {
+  const media = await Media.findAll({
+    //choose atribute
+    attributes:['id','image']
+  });
+
+  //change url image to ready use with host link
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  })
+
+  return res.json({
+    status: 'success',
+    data: mappedMedia
+  })
+});
+
 router.post('/',(req,res) => {
   const image = req.body.image;
 
@@ -13,7 +31,7 @@ router.post('/',(req,res) => {
     return res.status(400).json({status: 'error', message: 'invalid base64'});
   }
 
-  base64Img.img(image, 'public/images', Date.now(), async (err, filepath) => {
+  base64Img.img(image, './public/images', Date.now(), async (err, filepath) => {
     if(err){
       return res.status(400).json({status: 'error', message: err.message})
     }
@@ -21,7 +39,7 @@ router.post('/',(req,res) => {
     //public/images/asdasdasda.jpg
     const filename = filepath.split("\\").pop().split("/").pop();
 
-    const media = await Media.create({ image: `image/${filename}`});
+    const media = await Media.create({ image: `images/${filename}`});
 
     //langsung 200
     return res.json({
